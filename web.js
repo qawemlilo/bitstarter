eee49ab25f7345d4a7d760ef5366cc6105555a55
Raging Flame <qawemlilo@gmail.com>
2013-07-11 19:21:11 +0200

    Modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require('express'),
+    fs = require('fs');
 
-var app = express.createServer(express.logger());
+var app = express.createServer(express.logger()),
+    text = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(text.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
